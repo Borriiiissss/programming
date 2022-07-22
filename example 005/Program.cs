4e93e6bce69e7bc8719eb85652f92e7174596be6
Borriiiissss <chega123@mail.ru>
2022-07-22 15:19:21 +0200

    if else

---
+++ example 005/Program.cs
@@ -0,0 +1,13 @@
+﻿// exam 005
+
+Console.WriteLine("input your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "олег")
+{
+    Console.WriteLine("Ура это же Олег");
+}
+else
+{
+Console.WriteLine("Hello "+ username);
+}
