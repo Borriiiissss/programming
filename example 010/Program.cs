a6b0600c69480de6dd2d074ac47898158498d51f
Borriiiissss <chega123@mail.ru>
2022-07-23 14:34:37 +0200

    муть

--- example 010/Program.cs
+++ example 010/Program.cs
@@ -1,16 +1,16 @@
 ﻿// заполнение и вывод массивов с помощью функций
 int[] collection = new int[10];
+int f;
+
 void Fillarray (int[] collection)
 {
     int lenght = collection.Length;
     int index = 0;
     while (index < lenght)
     {
-        collection [index] = new Random().Next(1,1500000);
+        collection [index] = new Random().Next(1,5);
         index++;
     }
-    // System.Console.WriteLine(collection[]);
-
 }
 
 void Printarray (int[] collection)
@@ -24,9 +24,31 @@ void Printarray (int[] collection)
     }
 }
 
+int IndexOf (int[] collection, int find)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    int pos = -1;
+    while (index < lenght)
+    {
+        System.Console.WriteLine($"we are looking for {find}");
+        if (collection [index] == find)
+            
+            System.Console.WriteLine($"index of element = {index} ");
+            pos = index;
+            
+            
+        index++;
+    } 
+    return pos;
+}
 Fillarray (collection);
 Printarray (collection);
 
+System.Console.WriteLine("input number");
+string num = Console.ReadLine();
+int number = int.Parse(num);
+IndexOf (collection, number);
 
 
 
