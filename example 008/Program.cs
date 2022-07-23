ecbb498b39bcd152d9b52621c8abfc81ce98d5e3
Borriiiissss <chega123@mail.ru>
2022-07-23 10:19:31 +0200

    пример 008 с функцией

---
+++ example 008/Program.cs
@@ -0,0 +1,25 @@
+﻿// как работают функции
+
+int Max (int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+int a1 = 72;
+int b1 = 66662;
+int c1 = 452;
+int a2 = 23;
+int b2 = 112;
+int c2 = 22549;
+int a3 = 23;
+int b3 = 552;
+int c3 = 4;
+
+int max1 = Max (a1, b1, c1);
+int max2 = Max (a2, b2, c2);
+int max3 = Max (a3, b3, c3);
+
+System.Console.WriteLine(Max (max1, max2, max3));
