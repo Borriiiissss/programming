a8dbce726233418e6db7cf005c8824bd3aec3b12
Borriiiissss <chega123@mail.ru>
2022-07-23 13:45:41 +0200

    заполнении и вывод массивов с помощью функций

---
+++ example 010/Program.cs
@@ -0,0 +1,32 @@
+﻿// заполнение и вывод массивов с помощью функций
+int[] collection = new int[10];
+void Fillarray (int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection [index] = new Random().Next(1,1500000);
+        index++;
+    }
+    // System.Console.WriteLine(collection[]);
+
+}
+
+void Printarray (int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        System.Console.WriteLine(collection [index]);
+        index++;
+    }
+}
+
+Fillarray (collection);
+Printarray (collection);
+
+
+
+
